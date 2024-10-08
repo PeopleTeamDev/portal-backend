e2ea035a054ad35b9aa491fddaf8ec94d84f7e4d
TheSpagi <thespagi@icloud.com>
2024-10-08 06:05:56 +0200

    feat: Add basic nestjs api

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function main() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+}
+
+main();
