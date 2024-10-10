6425b9803fff9ccec2ce1714bb25721ec306fc4e
TheSpagi <thespagi@icloud.com>
2024-10-11 00:56:36 +0200

    chore: Add and configure docker

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from "./app.module";
 
 async function main() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+
+  const port = process.env.PORT
+  if (!port) throw new Error("PORT is not defined in .env file");
+  
   await app.listen(port);
 }
 
