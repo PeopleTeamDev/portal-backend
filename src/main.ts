914f4efe929ebc59db42cc2625d4d56ae52fdccf
TheSpagi <thespagi@icloud.com>
2024-10-12 18:23:44 +0200

    style: Update app.module.ts and main.ts to comply with formatting

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from "./app.module";
 async function main() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT
+  const port = process.env.PORT;
   if (!port) throw new Error("PORT is not defined in .env file");
-  
+
   await app.listen(port);
 }
 
