176be46adf3dd1b83390db0b91bc1a4007758160
TheSpagi <thespagi@icloud.com>
2024-10-15 04:19:14 +0200

    fix: Remove import of non-existing week module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
-import { WeekModule } from "./modules/week/week.module";
 import { DatabaseModule } from "./other/database/database.module";
 
 @Module({
